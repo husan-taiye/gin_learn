31c458e857926089e8ebb723f1687abe2ead2ee2
qilu <qilu@xuetangx.com>
2024-01-09 10:45:00 +0800

    cache

---
+++ webook/internal/repository/cache/user.go
@@ -0,0 +1,61 @@
+package cache
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"gin_learn/webook/internal/domain"
+	"github.com/redis/go-redis/v9"
+	"time"
+)
+
+var ErrUserNotFound = errors.New("用户缓存不存在")
+
+//type CacheV1 interface {
+//	Get(ctx context.Context, key string) (any error)
+//}
+
+type UserCache struct {
+	// 暂未实现	cache CacheV1
+	client     redis.Cmdable
+	expiration time.Duration
+}
+
+// A用到了B，B一定是接口
+// A用到了B，B一定是A的字段
+// A用到了B，A绝对不初始化B，而是外面注入
+
+// NewUserCache func NewUserCache(client redis.Cmdable, expiration time.Duration) UserCache {
+
+func NewUserCache(client redis.Cmdable) UserCache {
+	return UserCache{
+		client:     client,
+		expiration: time.Minute * 15,
+	}
+}
+func (cache *UserCache) Get(ctx context.Context, id int64) (domain.UserProfile, error) {
+	key := cache.key(id)
+	val, err := cache.client.Get(ctx, key).Bytes()
+	if err != nil {
+		return domain.UserProfile{}, err
+	}
+	var u domain.UserProfile
+	err = json.Unmarshal(val, &u)
+	if u == (domain.UserProfile{}) {
+		return u, ErrUserNotFound
+	}
+	return u, err
+}
+
+func (cache *UserCache) Set(ctx context.Context, user domain.UserProfile) error {
+	val, err := json.Marshal(user)
+	if err != nil {
+		return err
+	}
+	return cache.client.Set(ctx, cache.key(user.UserId), val, cache.expiration).Err()
+}
+
+func (cache *UserCache) key(id int64) string {
+	return fmt.Sprintf("user:info:%d", id)
+}
--- webook/internal/repository/user.go
+++ webook/internal/repository/user.go
@@ -2,22 +2,27 @@ package repository
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"gin_learn/webook/internal/domain"
+	"gin_learn/webook/internal/repository/cache"
 	"gin_learn/webook/internal/repository/dao"
 	"gin_learn/webook/utils"
 	"time"
 )
 
 type UserRepository struct {
-	dao *dao.UserDao
+	dao   *dao.UserDao
+	cache *cache.UserCache
 }
 
 var ErrUserDuplicateEmail = dao.ErrUserDuplicateEmail
 var ErrUserNotFount = dao.ErrUserNotFount
 
-func NewUserRepository(dao *dao.UserDao) *UserRepository {
+func NewUserRepository(dao *dao.UserDao, cache *cache.UserCache) *UserRepository {
 	return &UserRepository{
-		dao: dao,
+		dao:   dao,
+		cache: cache,
 	}
 }
 
@@ -52,14 +57,55 @@ func (ur *UserRepository) Update(ctx context.Context, up domain.UserProfile) err
 }
 
 func (ur *UserRepository) FindProfileByUserId(ctx context.Context, userId int64) (domain.UserProfile, error) {
-	up, err := ur.dao.FindProfileByUserId(ctx, userId)
-	if err != nil {
-		return domain.UserProfile{}, err
+	cacheUp, err := ur.cache.Get(ctx, userId)
+	//switch err {
+	//case nil:
+	//	return cacheUp, nil
+	//case ErrUserNotFount:
+	//	up, err := ur.dao.FindProfileByUserId(ctx, userId)
+	//	if err != nil {
+	//		return domain.UserProfile{}, err
+	//	}
+	//
+	//	toCacheUp := domain.UserProfile{
+	//		UserId:   up.UserId,
+	//		Nickname: up.Nickname,
+	//		Birthday: time.UnixMilli(up.Birthday).Format(utils.TimeTemplate3),
+	//		Profile:  up.Profile,
+	//	}
+	//	err = ur.cache.Set(ctx, toCacheUp)
+	//	if err != nil {
+	//		// 打日志，做监控
+	//		fmt.Sprintln("同步缓存失败")
+	//	}
+	//	return toCacheUp, err
+	//default:
+	//	return domain.UserProfile{}, nil
+	//}
+	if err == nil {
+		return cacheUp, nil
 	}
-	return domain.UserProfile{
-		UserId:   up.UserId,
-		Nickname: up.Nickname,
-		Birthday: time.UnixMilli(up.Birthday).Format(utils.TimeTemplate3),
-		Profile:  up.Profile,
-	}, nil
+	if errors.Is(err, cache.ErrUserNotFound) {
+		up, err := ur.dao.FindProfileByUserId(ctx, userId)
+		if err != nil {
+			return domain.UserProfile{}, err
+		}
+
+		toCacheUp := domain.UserProfile{
+			UserId:   up.UserId,
+			Nickname: up.Nickname,
+			Birthday: time.UnixMilli(up.Birthday).Format(utils.TimeTemplate3),
+			Profile:  up.Profile,
+		}
+		err = ur.cache.Set(ctx, toCacheUp)
+		if err != nil {
+			// 打日志，做监控
+			fmt.Sprintln("同步缓存失败")
+		}
+		return toCacheUp, err
+	}
+	// redis 其他错误，可能是崩了
+	// 保护数据库，数据库查询限流或者其他方式等等
+	// 这里暂时就先不查询了
+	return domain.UserProfile{}, nil
 }
